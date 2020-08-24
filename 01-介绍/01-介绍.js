119f6960e4e1b131962c1c65bc6891a20e36aca3
zzq5271137 <zzq5271137@yeah.net>
2020-08-24 23:53:00 +0800

    no message

---
+++ 01-介绍/01-介绍.js
@@ -0,0 +1,15 @@
+/**
+ * JavaScript简介
+ */
+
+/*
+ * JavaScript的组成:
+ * 1. ECMAScript:
+ *    JavaScript的标准库; ECMA是European Computer Manufacturers Association的缩写, 即欧洲计算机制造商协会;
+ *    ECMAScript是ECMA制定的脚本语言标准, 规定了一种脚本语言的实现应该包含的基本内容; JavaScript是脚本语言的一种,
+ *    所以JavaScript也必须遵守ECMAScript标准, 包含ECMAScript标准中规定的基本内容;
+ * 2. DOM(Document Object Model):
+ *    JavaScript操作网页上的元素(标签)的API;
+ * 3. BOM(Browser Object Model):
+ *    JavaScript操作浏览器的部分功能的API;
+ */
